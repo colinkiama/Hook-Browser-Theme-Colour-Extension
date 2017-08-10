539a7b66c5a144f1d04d9e28a1f973638102bfae
Colin Kiama <colinkiama@hotmail.co.uk>
2017-08-10 15:57:45 +0100

    First Release

---
+++ advancedTheme/themeService/blueTheme.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+
+
+
+namespace themeService
+{
+    public sealed class blueTheme: IBackgroundTask
+    {
+        private BackgroundTaskDeferral backgroundTaskDeferral;
+        private AppServiceConnection appServiceconnection;
+
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.backgroundTaskDeferral = taskInstance.GetDeferral(); // Get a deferral so that the service isn&#39;t terminated.
+            taskInstance.Canceled += OnTaskCanceled; // Associate a cancellation handler with the background task.
+
+            // Retrieve the app service connection and set up a listener for incoming app service requests.
+            var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            appServiceconnection = details.AppServiceConnection;
+            appServiceconnection.RequestReceived += OnRequestReceived;
+        }
+
+        private async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // Get a deferral because we use an awaitable API below to respond to the message
+            // and we dont want this call to get cancelled while we are waiting.
+            var messageDeferral = args.GetDeferral();
+
+            ValueSet message = args.Request.Message;
+            ValueSet returnData = new ValueSet();
+
+            string command = message["Command"] as string;
+
+            switch (command)
+            {
+                case "changeBackground":
+                    {
+                        
+                        returnData = createBlueColour();
+                        break;
+                    }
+               
+               
+                default:
+                    {
+                        break;
+                    }
+            }
+
+            await args.Request.SendResponseAsync(returnData); // Return the data to the caller.
+            messageDeferral.Complete(); // Complete the deferral so that the platform knows that we're done responding to the app service call.
+        }
+
+        private ValueSet createBlueColour()
+        {
+            ValueSet returnData = new ValueSet();
+            int[] blueARGB = new int[] { 255, 0, 0, 255 };
+            returnData.Add("colourARGB", blueARGB);
+            return returnData;
+        }
+
+        
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if (this.backgroundTaskDeferral != null)
+            {
+                // Complete the service deferral.
+                this.backgroundTaskDeferral.Complete();
+            }
+        }
+    }
+    }
